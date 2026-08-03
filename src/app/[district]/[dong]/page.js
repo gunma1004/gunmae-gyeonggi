8add00f0ec72b0f93225658c472132618714bd7e
gunma1004 <hanabank94@naver.com>
2026-08-03 16:19:37 +0900

    refactor: 불필요한 GyeonggiDongClientPage 파일 삭제

--- src/app/[district]/[dong]/page.js
+++ src/app/[district]/[dong]/page.js
@@ -67,18 +67,54 @@ const shops = [
 ];
 
 const gyeonggiDistricts = {
-  suwon: { name: '수원시' }, seongnam: { name: '성남시' }, goyang: { name: '고양시' },
-  yongin: { name: '용인시' }, bucheon: { name: '부천시' }, ansan: { name: '안산시' },
-  anyang: { name: '안양시' }, namyangju: { name: '남양주시' }, hwaseong: { name: '화성시' },
-  pyeongtaek: { name: '평택시' }, uijeongbu: { name: '의정부시' }, paju: { name: '파주시' },
-  gimpo: { name: '김포시' }, siheung: { name: '시흥시' }, gwangmyeong: { name: '광명시' },
-  gwangju: { name: '광주시' }, gunpo: { name: '군포시' }, osan: { name: '오산시' },
-  icheon: { name: '이천시' }, yangju: { name: '양주시' }, guri: { name: '구리시' },
-  anseong: { name: '안성시' }, pocheon: { name: '포천시' }, uiwang: { name: '의왕시' },
-  hanam: { name: '하남시' }, yeoju: { name: '여주시' }, yangpyeong: { name: '양평군' },
-  dongducheon: { name: '동두천시' }, gapyeong: { name: '가평군' }, gwacheon: { name: '과천시' },
-  yeoncheon: { name: '연천군' }
+  suwon: { name: '수원시', dongs: ['인계동', '영통동', '매탄동', '권선동', '조원동', '정자동', '파장동', '이의동', '원천동', '매교동'] },
+  seongnam: { name: '성남시', dongs: ['분당동', '야탑동', '서현동', '판교동', '모란동', '수내동', '정자동', '이매동', '태평동', '신흥동'] },
+  goyang: { name: '고양시', dongs: ['일산동', '백석동', '주엽동', '화정동', '행신동', '정발산동', '마두동', '대화동', '탄현동', '원당동'] },
+  yongin: { name: '용인시', dongs: ['기흥동', '수지동', '처인동', '동백동', '보정동', '풍덕천동', '죽전동', '상갈동', '신갈동', '역북동'] },
+  bucheon: { name: '부천시', dongs: ['중동', '상동', '심곡동', '원미동', '괴안동', '역곡동', '소사본동', '범박동', '오정동', '고강동'] },
+  ansan: { name: '안산시', dongs: ['중앙동', '고잔동', '선부동', '상록수동', '초지도', '본오동', '사동', '월피동', '와동'] },
+  anyang: { name: '안양시', dongs: ['안양동', '평촌동', '호계동', '비산동', '관양동', '박달동', '석수동', '범계동', '귀인동', '평안동'] },
+  namyangju: { name: '남양주시', dongs: ['다산동', '별내동', '진접동', '화도읍', '평내동', '호평동', '오남읍', '와부읍', '퇴계원읍', '진건읍'] },
+  hwaseong: { name: '화성시', dongs: ['동탄동', '병점동', '향남읍', '봉담읍', '새솔동', '반송동', '능동', '기안동', '정남면', '우정읍'] },
+  pyeongtaek: { name: '평택시', dongs: ['평택동', '비전동', '서정동', '안중읍', '팽성읍', '동삭동', '세교동', '고덕동', '포승읍', '송탄동'] },
+  uijeongbu: { name: '의정부시', dongs: ['의정부동', '호원동', '신곡동', '민락동', '용현동', '가능동', '녹양동', '금오동', '낙양동', '장암동'] },
+  paju: { name: '파주시', dongs: ['운정동', '금촌동', '문산읍', '야당동', '교하동', '동패동', '목동동', '탄현면', '법원읍', '파주읍'] },
+  gimpo: { name: '김포시', dongs: ['구래동', '장기동', '풍무동', '사우동', '운양동', '마산동', '통진읍', '고촌읍', '양촌읍', '하성면'] },
+  siheung: { name: '시흥시', dongs: ['정왕동', '대야동', '배곧동', '목감동', '은계동', '신천동', '월곶동', '장곡동', '하중동', '능곡동'] },
+  gwangmyeong: { name: '광명시', dongs: ['철산동', '하안동', '소하동', '광명동', '일직동', '학온동'] },
+  gwangju: { name: '광주시', dongs: ['오포동', '초월읍', '퇴촌면', '경안동', '곤지암읍', '태전동', '쌍령동', '도척면', '중부면'] },
+  gunpo: { name: '군포시', dongs: ['산본동', '금정동', '당동', '부곡동', '대야미동', '재궁동', '오금동', '수리동', '송부동'] },
+  osan: { name: '오산시', dongs: ['오산동', '궐동', '원동', '세교동', '남촌동', '초평동', '대원동', '누읍동', '가수동'] },
+  icheon: { name: '이천시', dongs: ['창전동', '증포동', '부발읍', '마장면', '중리동', '관고동', '백사면', '호법면', '장호원읍'] },
+  yangju: { name: '양주시', dongs: ['회천동', '고읍동', '옥정동', '덕계동', '백석읍', '장흥면', '남면', '은현면', '양주동'] },
+  guri: { name: '구리시', dongs: ['인창동', '수택동', '토평동', '교문동', '갈매동', '동구동', '아천동'] },
+  anseong: { name: '안성시', dongs: ['공도읍', '안성동', '대덕면', '고삼면', '일죽면', '죽산면', '삼죽면', '보개면', '금광면'] },
+  pocheon: { name: '포천시', dongs: ['소흘읍', '포천동', '선단동', '가산면', '신북면', '창수면', '영중면', '일동면', '이동면'] },
+  uiwang: { name: '의왕시', dongs: ['내손동', '부곡동', '오전동', '청계동', '고천동', '포일동', '월암동'] },
+  hanam: { name: '하남시', dongs: ['미사동', '풍산동', '위례동', '신장동', '덕풍동', '감일동', '창우동', '초이동', '망월동'] },
+  yeoju: { name: '여주시', dongs: ['여흥동', '중앙동', '오학동', '가남읍', '점동면', '능서면', '대신면', '북내면', '강천면'] },
+  yangpyeong: { name: '양평군', dongs: ['양평읍', '용문면', '강상면', '서종면', '지평면', '옥천면', '단월면', '양서면', '강하면'] },
+  dongducheon: { name: '동두천시', dongs: ['생연동', '보산동', '지행동', '상패동', '중앙동', '송내동', '불현동', '소요동'] },
+  gapyeong: { name: '가평군', dongs: ['가평읍', '청평면', '설악면', '조종면', '상면', '북면'] },
+  gwacheon: { name: '과천시', dongs: ['별양동', '중앙동', '문원동', '갈현동', '부림동', '과천동', '원문동'] },
+  yeoncheon: { name: '연천군', dongs: ['연천읍', '전곡읍', '군남면', '청산면', '미산면', '왕징면', '신서면', '중면', '장남면'] }
 };
+
+// Next.js 빌드 시점에 모든 시·군과 동의 조합 경로를 미리 생성해 줍니다.
+export async function generateStaticParams() {
+  const paths = [];
+  for (const [districtKey, data] of Object.entries(gyeonggiDistricts)) {
+    if (data.dongs) {
+      for (const dong of data.dongs) {
+        paths.push({
+          district: districtKey,
+          dong: dong,
+        });
+      }
+    }
+  }
+  return paths;
+}
 
 export default function DongPage({ params }) {
   const resolvedParams = use(params);
