9af54f01745e2eb6ea767a24aaed2514c1ff38c2
gunma1004 <hanabank94@naver.com>
2026-08-11 16:08:40 +0900

    사이트맵 변경

--- src/app/robots.js
+++ src/app/robots.js
@@ -2,6 +2,7 @@ export default function robots() {
   return {
     userAgent: '*',
     allow: '/',
+    // ⭐️ 이전 주소(.com)로 되어 있다면 현재 주소(.shop)로 변경해 주세요!
     sitemap: 'https://gunmalove-gyeonggi.shop/sitemap.xml',
   };
 }
